6f520de6a755c49d3678aa939adad284a2c0b14c
Shurxy <ziheng.zj@gmail.com>
2025-05-26 18:03:07 +0200

    0.2 - Interface + API Calls (logik in app1)

--- TU_fy/TU_fy/settings.py
+++ TU_fy/TU_fy/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = "TU_fy.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [BASE_DIR, 'template'],
+        "DIRS": [BASE_DIR, 'templates'],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
--- TU_fy/app1/views.py
+++ TU_fy/app1/views.py
@@ -1,12 +1,34 @@
 #functions wie get blablabla
 
-from django.shortcuts import render, redirect
 
-from app1.models import Request
-from template import *
 
+import requests
+from django.shortcuts import render
+
+
+#Send request func
 def index(request):
-    artist= Request.objects.all()
-    return render(request, "index.html" ,{'artist': artist} )
+    lyrics = None
+    error = None
+
+    artist = request.GET.get('artist')
+    title = request.GET.get('title')
+
+    if artist and title:
+        api_url = f"https://api.lyrics.ovh/v1/{artist}/{title}"
+        response = requests.get(api_url)
+
+        if response.status_code == 200:
+            data = response.json()
+            lyrics = data.get('lyrics', 'Keine Lyrics gefunden.')
+        else:
+            error = "Songtext nicht gefunden oder Fehler bei der API."
+
+    return render(request, 'index.html', {
+        'lyrics': lyrics,
+        'error': error
+    })
 
 
+def imprint(request):
+    return render(request, 'imprint.html')
