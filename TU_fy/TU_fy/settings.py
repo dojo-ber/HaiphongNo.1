9ad3e7aeae52e8c2a81d816692133c7ef66382f9
Kevin Chu <kevchu@MacBook-Air-von-Kevin.local>
2025-05-26 19:20:03 +0200

    App2 User Login + registration + templates(login, register, dashboard)

--- TU_fy/TU_fy/settings.py
+++ TU_fy/TU_fy/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "app2",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -120,3 +121,6 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# LOGIN REDIRECT
+LOGIN_REDIRECT_URL = 'dashboard'
--- TU_fy/app2/views.py
+++ TU_fy/app2/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+# Create your views here.
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Konto wurde erfolgreich erstellt! Du kannst dich jetzt einloggen.')
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'users/register.html', {'form': form})
+
+
+
+@login_required
+def dashboard(request):
+    return render(request, 'users/dashboard.html', {'user': request.user})
