c32085531cd00e6973cd82271128c1043191818f
kevkorn <duongber@googlemail.com>
2025-05-27 23:00:35 +0200

    Kommentare gelöscht

--- TU_fy/app2/urls.py
+++ TU_fy/app2/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='index'), name='logout'),
-    path('dashboard/', views.dashboard, name='dashboard'),  # 👈 Neue Route
+    path('dashboard/', views.dashboard, name='dashboard'),  
 
 ]
