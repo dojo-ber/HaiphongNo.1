46a12d6f2dbe8f24767fc151628c561059d10c98
Kevin Chu <kevchu@MacBook-Air-von-Kevin.fritz.box>
2025-06-03 12:55:58 +0200

    UI/UX verbessert + Delete Condirmation

--- TU_fy/app3/urls.py
+++ TU_fy/app3/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
-    path("create_playlist", views.PlayListCreateView.as_view(), name="create_playlist"),
-    path("overview_playlist", views.PlayListOverview.as_view(), name="overview_playlist"),
-    path("detail_playlist/<int:playlist_id>", views.playlist_detail, name="detail_playlist")
+    path('create/', views.PlaylistCreateView.as_view(), name='create_playlist'),
+    path('overview/', views.PlaylistOverview.as_view(), name='overview_playlist'),
+    path('detail/<int:playlist_id>/', views.playlist_detail, name='detail_playlist'),
+    path('delete/<int:pk>/', views.PlaylistDeleteView.as_view(), name='delete_playlist'),
 ]
--- TU_fy/app3/views.py
+++ TU_fy/app3/views.py
@@ -2,24 +2,26 @@ from django.urls import reverse_lazy
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib import messages
 from django.views import generic
 from .models import Playlist, PlaylistSong, Song
 from .forms import PlaylistForm
 
 
 # View Classes machen das ganze viel einfacher
-class PlayListCreateView(LoginRequiredMixin, generic.CreateView):
+class PlaylistCreateView(LoginRequiredMixin, generic.CreateView):
     model = Playlist
     form_class = PlaylistForm
     template_name = 'playlist/create_playlist.html'
-    success_url = reverse_lazy('dashboard')
+    success_url = reverse_lazy('overview_playlist')
     
     def form_valid(self, form):
+        messages.success(self.request, "The playlist was created successfully.")
         form.instance.creator = self.request.user
         return super().form_valid(form)
     
     
-class PlayListOverview(LoginRequiredMixin, generic.ListView):
+class PlaylistOverview(LoginRequiredMixin, generic.ListView):
     model = Playlist
     template_name = 'playlist/overview_playlist.html'
     context_object_name = 'all_playlists'
@@ -38,3 +40,16 @@ def playlist_detail(request, playlist_id):
         'playlist': playlist,
         'songs': songs,
     })
+    
+class PlaylistDeleteView(LoginRequiredMixin, generic.DeleteView):
+    model = Playlist
+    template_name = 'playlist/delete_playlist.html'
+    context_object_name = 'playlist'
+    success_url = reverse_lazy('overview_playlist')
+
+    def get_queryset(self):
+        return Playlist.objects.filter(creator=self.request.user)
+
+    def form_valid(self, form):
+        messages.success(self.request, "The playlist was deleted successfully.")
+        return super().form_valid(form)
